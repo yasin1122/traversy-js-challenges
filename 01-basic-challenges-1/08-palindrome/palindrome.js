d6b3cdb40ef5b985fe5728fc7308627e0fdc0e92
Yasin Cakal <whoisyasin@gmail.com>
2025-01-30 11:38:21 -0800

    palindrome

--- 01-basic-challenges-1/08-palindrome/palindrome.js
+++ 01-basic-challenges-1/08-palindrome/palindrome.js
@@ -1,3 +1,10 @@
-function isPalindrome() {}
+function isPalindrome(str) {
+  const formattedStr = str.toLowerCase().replace(/[^a-z0-9]/g, '')
+  for (let i = 0, j = formattedStr.length - 1; i < j; i++, j--) {
+    console.log(formattedStr[i], formattedStr[j])
+    if (formattedStr[i] !== formattedStr[j]) return false
+  }
+  return true
+}
 
-module.exports = isPalindrome;
+module.exports = isPalindrome
