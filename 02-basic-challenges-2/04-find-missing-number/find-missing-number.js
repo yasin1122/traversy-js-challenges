09934f71c6945e177b172befc041e3272dd18b9f
Yasin Cakal <whoisyasin@gmail.com>
2025-01-30 14:16:22 -0800

    missing number

--- 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
+++ 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
@@ -1,3 +1,20 @@
-function findMissingNumber() {}
+function findMissingNumber(nums) {
+  if (!nums) return undefined
+  else if (nums.length === 0) return 1
+  else {
+    // let total = 1
+    // let missingTotal = 0
+    // for (let i = 1; i <= nums.length; i++) {
+    //   total += i + 1
+    //   missingTotal += nums[i - 1]
+    // }
+    // console.log(total, missingTotal)
 
-module.exports = findMissingNumber;
+    let total = ((nums.length + 1) * (nums.length + 2)) / 2
+    let missingTotal = nums.reduce((sum, num) => sum + num, 0)
+
+    return total - missingTotal
+  }
+}
+
+module.exports = findMissingNumber
