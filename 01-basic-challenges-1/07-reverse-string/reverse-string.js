5074c7d36cd626d423709aced8f74add6b103b57
Yasin Cakal <whoisyasin@gmail.com>
2025-01-30 09:23:41 -0800

    reverse

--- 01-basic-challenges-1/07-reverse-string/reverse-string.js
+++ 01-basic-challenges-1/07-reverse-string/reverse-string.js
@@ -1,9 +1,10 @@
 function reverseString(str) {
-  let reversed = ''
-  for (let i = str.length - 1; i >= 0; i--) {
-    reversed += str[i]
-  }
-  return reversed
+  // let reversed = ''
+  // for (let i = str.length - 1; i >= 0; i--) {
+  //   reversed += str[i]
+  // }
+  // return reversed
+  return str.split('').reverse().join('')
 }
 
 module.exports = reverseString
