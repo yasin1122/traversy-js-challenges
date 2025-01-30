a1ef523c1857df4dbb32bcb8a545241c14da6315
Yasin Cakal <whoisyasin@gmail.com>
2025-01-30 13:14:43 -0800

    intersect

--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,3 +1,18 @@
-function arrayIntersection() {}
+// function arrayIntersection(arr1, arr2) {
+//   let arr3 = []
+//   for (let i = 0; i < arr1.length; i++) {
+//     if (arr2.includes(arr1[i])) arr3.push(arr1[i])
+//   }
+//   return arr3
+// }
 
-module.exports = arrayIntersection;
+const arrayIntersection = (arr1, arr2) => {
+  const set1 = new Set(arr1)
+  const arr3 = []
+  for (let num of arr2) {
+    if (set1.has(num)) arr3.push(num)
+  }
+  return arr3
+}
+
+module.exports = arrayIntersection
