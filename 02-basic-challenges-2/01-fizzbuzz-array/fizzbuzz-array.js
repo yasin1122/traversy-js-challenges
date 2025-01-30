acb810db6c333b8d07e5c326545b8df93ad8a7b1
Yasin Cakal <whoisyasin@gmail.com>
2025-01-30 13:03:32 -0800

    fizzbuzz

--- 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
+++ 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
@@ -1,3 +1,12 @@
-function fizzBuzzArray() {}
+function fizzBuzzArray(num) {
+  let arr = []
+  for (let i = 1; i <= num; i++) {
+    if (i % 3 === 0 && i % 5 === 0) arr.push('FizzBuzz')
+    else if (i % 3 === 0) arr.push('Fizz')
+    else if (i % 5 === 0) arr.push('Buzz')
+    else arr.push(i)
+  }
+  return arr
+}
 
-module.exports = fizzBuzzArray;
+module.exports = fizzBuzzArray
