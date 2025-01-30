694be9f4247e2db4a5e86fe2200f67d13a5b6f49
Yasin Cakal <whoisyasin@gmail.com>
2025-01-30 09:18:53 -0800

    reverse

--- 01-basic-challenges-1/06-title-case/title-case.js
+++ 01-basic-challenges-1/06-title-case/title-case.js
@@ -1,10 +1,15 @@
 function titleCase(str) {
-  let words = str.toLowerCase().split(' ')
-  for (let word of words) {
-    word[0].toUpperCase()
-  }
-  words.join(' ')
-  return words
+  // let words = str.toLowerCase().split(' ')
+  // for (let i = 0; i < words.length; i++) {
+  //   words[i] = words[i][0].toUpperCase() + words[i].slice(1)
+  // }
+  // words = words.join(' ')
+  // return words
+  return str
+    .toLowerCase()
+    .split(' ')
+    .map(word => word[0].toUpperCase() + word.slice(1))
+    .join(' ')
 }
 
 module.exports = titleCase
--- 01-basic-challenges-1/07-reverse-string/reverse-string.js
+++ 01-basic-challenges-1/07-reverse-string/reverse-string.js
@@ -1,3 +1,9 @@
-function reverseString() {}
+function reverseString(str) {
+  let reversed = ''
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed += str[i]
+  }
+  return reversed
+}
 
-module.exports = reverseString;
+module.exports = reverseString
