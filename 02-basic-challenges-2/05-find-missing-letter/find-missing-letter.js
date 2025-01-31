1d15b5d23f6cfa4cc94ddb11221a8d8f1ce63c6f
Yasin Cakal <whoisyasin@gmail.com>
2025-01-31 11:18:37 -0800

    find ch

--- 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
+++ 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
@@ -1,3 +1,27 @@
-function findMissingLetter() {}
+// function findMissingLetter(arr) {
+//   const alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+//   const startIndex = alphabet.indexOf(arr[0])
 
-module.exports = findMissingLetter;
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] !== alphabet[startIndex + i]) {
+//       return alphabet[startIndex + i]
+//     }
+//   }
+//   return ''
+// }
+
+const findMissingLetter = arr => {
+  let start = arr[0].charCodeAt(0)
+
+  for (let i = 0; i < arr.length; i++) {
+    const current = arr[i].charCodeAt(0)
+
+    if (current - start > 1) {
+      return String.fromCharCode(start + 1)
+    }
+
+    start = current
+  }
+  return ''
+}
+module.exports = findMissingLetter
