3ed88fd7233a19bd1c4c35270f4398aad5a6a3de
Yasin Cakal <whoisyasin@gmail.com>
2025-02-01 10:42:51 -0800

    dicegame

--- 02-basic-challenges-2/08-dice-game/dice-game.js
+++ 02-basic-challenges-2/08-dice-game/dice-game.js
@@ -1,3 +1,22 @@
-function diceGameSimulation() {}
+const rollDie = () => Math.floor(Math.random() * 6 + 1)
 
-module.exports = diceGameSimulation;
+const playOnce = () => {
+  const die1 = rollDie()
+  const die2 = rollDie()
+  const sum = die1 + die2
+  let result = ''
+  if (sum === 7 || sum === 11) result = 'win'
+  else if (sum === 2 || sum === 3 || sum === 12) result = 'lose'
+  else result = 'roll again'
+  return { dice1: die1, dice2: die2, sum: sum, result: result }
+}
+
+function diceGameSimulation(numSimulations) {
+  let resultArr = []
+  for (let i = 0; i < numSimulations; i++) {
+    resultArr.push(playOnce())
+  }
+  return resultArr
+}
+
+module.exports = diceGameSimulation
