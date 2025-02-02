7938e6ac102d7d7231584246ae2282f56924b27e
Yasin Cakal <whoisyasin@gmail.com>
2025-02-01 22:02:49 -0800

    review

--- 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
+++ 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
@@ -1,3 +1,9 @@
-function sumOfEvenSquares() {}
+function sumOfEvenSquares(nums) {
+  let evenNums = nums.filter(num => num % 2 === 0)
+  let squaredEvens = evenNums.map(num => num * num)
+  // The optional 0 resolves the issue with an empty nums[]
+  let total = squaredEvens.reduce((sum, num) => sum + num, 0)
+  return total
+}
 
-module.exports = sumOfEvenSquares;
+module.exports = sumOfEvenSquares
