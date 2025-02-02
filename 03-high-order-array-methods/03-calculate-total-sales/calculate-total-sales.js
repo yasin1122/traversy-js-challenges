a57482538ca912605ac6f86843639c242221680b
Yasin Cakal <whoisyasin@gmail.com>
2025-02-02 09:26:34 -0800

    total sales

--- 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
+++ 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
@@ -1,3 +1,12 @@
-function calculateTotalSalesWithTax() {}
+function calculateTotalSalesWithTax(products, taxRate) {
+  const totalSales = products.reduce((sum, product) => {
+    return sum + product.price * product.quantity
+  }, 0)
 
-module.exports = calculateTotalSalesWithTax;
+  const taxAmount = (taxRate * totalSales) / 100
+  const totalSalesWithTax = totalSales + taxAmount
+
+  return parseFloat(totalSalesWithTax.toFixed(2))
+}
+
+module.exports = calculateTotalSalesWithTax
