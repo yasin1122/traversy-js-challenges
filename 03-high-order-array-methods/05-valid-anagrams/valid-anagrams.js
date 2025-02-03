6d727d9b579af1fc4749d9165349f1123ea94c5a
Yasin Cakal <whoisyasin@gmail.com>
2025-02-02 18:32:44 -0800

    anagram

--- 03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
+++ 03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
@@ -1,3 +1,21 @@
-function validAnagrams() {}
+// function validAnagrams(str1, str2) {
+//   return str1.split('').sort().join('') === str2.split('').sort().join('')
+// }
 
-module.exports = validAnagrams;
+const validAnagrams = (str1, str2) => {
+  const freqCount1 = str1.split('').reduce((acc, char) => {
+    acc[char] = (acc[char] || 0) + 1
+    return acc
+  }, {})
+
+  const freqCount2 = str2.split('').reduce((acc, char) => {
+    acc[char] = (acc[char] || 0) + 1
+    return acc
+  }, {})
+
+  return Object.keys(freqCount1).every(char => {
+    return freqCount1[char] === freqCount2[char]
+  })
+}
+
+module.exports = validAnagrams
