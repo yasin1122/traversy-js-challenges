5bd26a2f0a1ca4b3c89b24bc474d587245d6a6a1
Yasin Cakal <whoisyasin@gmail.com>
2025-02-03 08:56:05 -0800

    recursion

--- 03-high-order-array-methods/10-find-missing-letter-refactor/find-missing-letter-refactor.js
+++ 03-high-order-array-methods/10-find-missing-letter-refactor/find-missing-letter-refactor.js
@@ -1,20 +1,15 @@
 function findMissingLetter(arr) {
-  // `start` is the character code of the first character in the array
   let start = arr[0].charCodeAt(0)
-  // `missingCharCode` is the character code of the missing character
+
   const missingCharCode = arr
-    .map(char => char.charCodeAt(0)) // convert each character to its character code
-    // find the first character code that is not one more than the previous character code
+    .map(char => char.charCodeAt(0))
     .find(current => {
-      //  if the current character code is more than one greater than the previous character code then return true
       if (current - start > 1) {
         return true
       }
-      // otherwise set `start` to the current character code and return false
       start = current
       return false
     })
-  // if `missingCharCode` is truthy then return the character that is one less than the character code
   return missingCharCode ? String.fromCharCode(missingCharCode - 1) : ''
 }
 module.exports = findMissingLetter
--- 04-recursion/01-recursion-intro/count-down.js
+++ 04-recursion/01-recursion-intro/count-down.js
@@ -1,3 +1,11 @@
-function countDown() {}
+function countDown(num) {
+  if (num <= 0) {
+    console.log('All done!')
+    return
+  }
+  console.log(num)
+  num--
+  countDown(num)
+}
 
-module.exports = countDown;
+module.exports = countDown
