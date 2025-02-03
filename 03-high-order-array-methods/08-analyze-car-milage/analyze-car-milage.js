d6b3fb2616d27873bc69bfdef15ef8e8266ad46e
Yasin Cakal <whoisyasin@gmail.com>
2025-02-02 22:19:18 -0800

    mileage

--- 03-high-order-array-methods/08-analyze-car-milage/analyze-car-milage.js
+++ 03-high-order-array-methods/08-analyze-car-milage/analyze-car-milage.js
@@ -1,3 +1,49 @@
-function analyzeCarMileage() {}
+// function analyzeCarMileage(cars) {
+//   // Start both highest/lowest as the first car
+//   let highestMileageCar = cars[0]
+//   let lowestMileageCar = cars[0]
 
-module.exports = analyzeCarMileage;
+//   const totalMileage = cars.reduce((total, car) => {
+//     if (car.mileage > highestMileageCar.mileage) {
+//       highestMileageCar = car
+//     }
+//     if (car.mileage < lowestMileageCar.mileage) {
+//       lowestMileageCar = car
+//     }
+//     return total + car.mileage
+//   }, 0)
+
+//   const averageMileage = parseFloat((totalMileage / cars.length).toFixed(2))
+
+//   return {
+//     averageMileage, // e.g., 23333.33 (as a number, not a string)
+//     highestMileageCar, // the car with the largest .mileage
+//     lowestMileageCar, // the car with the smallest .mileage
+//     totalMileage // sum of all mileages
+//   }
+// }
+
+function analyzeCarMileage(cars) {
+  const totalMileage = cars.reduce((sum, car) => sum + car.mileage, 0)
+
+  const averageMileage = totalMileage / cars.length
+
+  const highestMileageCar = cars.reduce(
+    (highest, car) => (car.mileage > highest.mileage ? car : highest),
+    cars[0]
+  )
+
+  const lowestMileageCar = cars.reduce(
+    (lowest, car) => (car.mileage < lowest.mileage ? car : lowest),
+    cars[0]
+  )
+
+  return {
+    averageMileage: parseFloat(averageMileage.toFixed(2)),
+    highestMileageCar,
+    lowestMileageCar,
+    totalMileage
+  }
+}
+
+module.exports = analyzeCarMileage
