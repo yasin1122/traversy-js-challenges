6f767fea27f0bd1af019585ea5d1218677fd8792
Yasin Cakal <whoisyasin@gmail.com>
2025-02-03 09:38:20 -0800

    call stack

--- 04-recursion/02-unwinding/sum-up-to.js
+++ 04-recursion/02-unwinding/sum-up-to.js
@@ -1,3 +1,14 @@
-function sumUpTo() {}
+function sumUpTo(n) {
+  // Base case: for 0 and 1, just return n (0 for n=0, 1 for n=1)
+  // console.log(n)
+  if (n <= 1) {
+    return n
+  }
+  // Recursive case
+  let recursive = sumUpTo(n - 1)
+  let output = n + recursive
+  // console.log('output: ', output, 'recursive: ', recursive)
+  return output
+}
 
-module.exports = sumUpTo;
+module.exports = sumUpTo
