657e9b0735d8de24aa25b20cd88fed67facfc925
Yasin Cakal <whoisyasin@gmail.com>
2025-02-02 19:31:21 -0800

    hashtags

--- 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
+++ 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
@@ -1,3 +1,24 @@
-function generateHashtag() {}
+// function generateHashtag(str) {
+//   if (str.length < 1 || str.length > 140) return false
 
-module.exports = generateHashtag;
+//   return (
+//     '#' +
+//     str
+//       .trim()
+//       .split(/\s+/) // Regex for whitespace
+//       .map(word => {
+//         return word.charAt(0).toUpperCase() + word.slice(1)
+//       })
+//       .join('')
+//   )
+// }
+
+const generateHashtag = str => {
+  return str.length < 1 || str.length > 140
+    ? false
+    : str.split(' ').reduce(function (tag, word) {
+        return tag + word.charAt(0).toUpperCase() + word.substring(1)
+      }, '#')
+}
+
+module.exports = generateHashtag
