42e49c9d4bdd44ea0146e63741dce7b7784f31c3
Yasin Cakal <whoisyasin@gmail.com>
2025-02-03 22:07:56 -0800

    sum

--- 04-recursion/07-array-sum/array-sum.js
+++ 04-recursion/07-array-sum/array-sum.js
@@ -1,3 +1,19 @@
-function arraySum() {}
+function arraySum(arr) {
+  if (arr.length === 1) return arr[0]
 
-module.exports = arraySum;
+  return arraySum(arr.slice(1)) + arr[0]
+}
+
+module.exports = arraySum
+
+/*
+
+[2, 3, 4] + 1
+[3, 4] + 2
+[4] + 3
+
+4 + 3 = 7
+7 + 2 = 9
+9 + 1 = 10
+
+*/
