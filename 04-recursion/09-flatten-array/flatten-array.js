00c9f125f0f352f2faca68dc95a28b5fb8d4627c
Yasin Cakal <whoisyasin@gmail.com>
2025-02-03 22:21:34 -0800

    flatten

--- 04-recursion/09-flatten-array/flatten-array.js
+++ 04-recursion/09-flatten-array/flatten-array.js
@@ -1,3 +1,15 @@
-function flattenArray() {}
+function flattenArray(arr) {
+  let result = []
 
-module.exports = flattenArray;
+  for (const item of arr) {
+    if (Array.isArray(item)) {
+      result = result.concat(flattenArray(item))
+    } else {
+      result.push(item)
+    }
+  }
+
+  return result
+}
+
+module.exports = flattenArray
