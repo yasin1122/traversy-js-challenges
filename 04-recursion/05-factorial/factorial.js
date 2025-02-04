6335a3de77bb72804336833f0a64467a994d77bd
Yasin Cakal <whoisyasin@gmail.com>
2025-02-03 21:58:57 -0800

    exp

--- 04-recursion/05-factorial/factorial.js
+++ 04-recursion/05-factorial/factorial.js
@@ -1,7 +1,9 @@
-function factorial(num) {
-  if (num < 2) return 1
+// function factorial(num) {
+//   if (num < 2) return 1
 
-  return factorial(num - 1) * num
-}
+//   return factorial(num - 1) * num
+// }
+
+const factorial = n => (n < 2 ? 1 : factorial(n - 1) * n)
 
 module.exports = factorial
--- 04-recursion/06-power/power.js
+++ 04-recursion/06-power/power.js
@@ -1,3 +1,9 @@
-function power() {}
+// function power(base, exp) {
+//   if (exp === 0) return 1
 
-module.exports = power;
+//   return power(base, exp - 1) * base
+// }
+
+const power = (base, exp) => (exp === 0 ? 1 : power(base, exp - 1) * base)
+
+module.exports = power
