22e549de8000768da3b7f0de3c836d11d20b841a
Yasin Cakal <whoisyasin@gmail.com>
2025-02-04 21:33:01 -0800

    maps

--- 05-complexity/10-max-subarray-linear/max-subarray-linear.js
+++ 05-complexity/10-max-subarray-linear/max-subarray-linear.js
@@ -9,9 +9,11 @@ function maxSubarraySum(arr, k) {
   currentSum = maxSum
 
   for (let i = k; i < arr.length; i++) {
+    console.log(currentSum, arr[i - k], arr[i])
     currentSum = currentSum - arr[i - k] + arr[i]
 
-    // console.log(`${currentSum} - ${arr[i - k]} + ${arr[i]}`)
+    // const arr = [2, 5, 3, 1, 11, 7, 6, 4]
+    console.log(`${currentSum} - ${arr[i - k]} + ${arr[i]}`)
 
     maxSum = Math.max(maxSum, currentSum)
   }
--- 06-hash-tables-maps-sets/03-maps/maps-done.js
+++ 06-hash-tables-maps-sets/03-maps/maps-done.js
@@ -1,11 +1,11 @@
 const nameMap = new Map([
   [1, 'John'],
   [2, 'Jane'],
-  [3, 'Joe'],
-]);
+  [3, 'Joe']
+])
 
-const myFunction = () => {};
-const emptyObj = {};
+const myFunction = () => {}
+const emptyObj = {}
 
 const map2 = new Map([
   ['name', 'John'],
@@ -13,45 +13,45 @@ const map2 = new Map([
   [true, 'really true'],
   [null, 'null'],
   [myFunction, 'empty function'],
-  [emptyObj, 'empty object'],
-]);
+  [emptyObj, 'empty object']
+])
 
 // Getting values
-console.log(nameMap.get(1));
-console.log(map2.get(myFunction));
-console.log(map2.get(emptyObj));
+console.log(nameMap.get(1))
+console.log(map2.get(myFunction))
+console.log(map2.get(emptyObj))
 
 // Setting values
-nameMap.set(4, 'Jack');
-nameMap.set(5, 'Jill');
+nameMap.set(4, 'Jack')
+nameMap.set(5, 'Jill')
 
 // Checking values
-console.log(nameMap.has(1));
-console.log(nameMap.has(6));
+console.log(nameMap.has(1))
+console.log(nameMap.has(6))
 
 // Deleting values
-nameMap.delete(1);
-console.log(nameMap.has(1));
+nameMap.delete(1)
+console.log(nameMap.has(1))
 
 // Get Size
-console.log(nameMap.size);
+console.log(nameMap.size)
 
 // Iterating (for...of)
 for (let [key, value] of nameMap) {
-  console.log(key, value);
+  console.log(key, value)
 }
 
 // Using forEach
 nameMap.forEach((value, key) => {
-  console.log(key, value);
-});
+  console.log(key, value)
+})
 
 // Looping keys and values
-console.log(nameMap.keys());
-console.log(nameMap.values());
+console.log(nameMap.keys())
+console.log(nameMap.values())
 
 // Clearing
-nameMap.clear();
-console.log(nameMap.size);
+nameMap.clear()
+console.log(nameMap.size)
 
 // console.log(nameMap);
