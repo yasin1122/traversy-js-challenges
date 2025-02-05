c306af326ebfdae0864baed15494c5244fb9482e
Yasin Cakal <whoisyasin@gmail.com>
2025-02-04 18:14:01 -0800

    max subarray

--- 05-complexity/08-max-subarray-quadratic/max-subarray-quadratic.js
+++ 05-complexity/08-max-subarray-quadratic/max-subarray-quadratic.js
@@ -1,3 +1,14 @@
-function maxSubarraySum() {}
+function maxSubarraySum(arr, k) {
+  let maxSum = -Infinity
 
-module.exports = maxSubarraySum;
+  for (let i = 0; i <= arr.length - k; i++) {
+    let currentSum = 0
+    for (let j = i; j < i + k; j++) {
+      currentSum += arr[j]
+    }
+    maxSum = Math.max(maxSum, currentSum)
+  }
+  return maxSum
+}
+
+module.exports = maxSubarraySum
