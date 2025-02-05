092ad8846d28e698e6f84a0237e944472890d6ee
Yasin Cakal <whoisyasin@gmail.com>
2025-02-05 07:49:39 -0800

    word freq

--- 06-hash-tables-maps-sets/04-word-frequency-counter/word-frequency-counter.js
+++ 06-hash-tables-maps-sets/04-word-frequency-counter/word-frequency-counter.js
@@ -1,3 +1,19 @@
-function wordFrequencyCounter() {}
+function wordFrequencyCounter(str) {
+  const words = str.toLowerCase().split(/\W+/)
 
-module.exports = wordFrequencyCounter;
+  const wordFrequency = new Map()
+
+  for (const word of words) {
+    if (word === '') continue
+
+    if (wordFrequency.has(word)) {
+      wordFrequency.set(word, wordFrequency.get(word) + 1)
+    } else {
+      wordFrequency.set(word, 1)
+    }
+  }
+
+  return wordFrequency
+}
+
+module.exports = wordFrequencyCounter
