22cd0731e3c1b456c70d0bed5bb17cef0b6318a9
Yasin Cakal <whoisyasin@gmail.com>
2025-02-05 19:17:05 -0800

    n

--- 06-hash-tables-maps-sets/09-two-sum/two-sum.js
+++ 06-hash-tables-maps-sets/09-two-sum/two-sum.js
@@ -1,11 +1,26 @@
-function twoSum(arr, target) {
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr.length; j++) {
-      if (j === i) continue
-      else if (arr[j] + arr[i] === target) return [i, j]
-    }
-  }
+// function twoSum(arr, target) {
+//   for (let i = 0; i < arr.length; i++) {
+//     for (let j = 0; j < arr.length; j++) {
+//       if (j === i) continue
+//       else if (arr[j] + arr[i] === target) return [i, j]
+//     }
+//   }
 
+//   return []
+// }
+
+// O(n) Solution
+const twoSum = (nums, target) => {
+  const numsMap = new Map()
+
+  for (let i = 0; i < nums.length; i++) {
+    const curr = nums[i]
+    const diff = target - curr
+
+    if (numsMap.has(diff)) return [numsMap.get(diff), i]
+
+    numsMap.set(curr, i)
+  }
   return []
 }
 
