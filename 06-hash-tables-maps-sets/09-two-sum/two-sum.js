e836774e7fccc12f8f3167fcc6537d9147323f83
Yasin Cakal <whoisyasin@gmail.com>
2025-02-05 19:09:40 -0800

    twosum

--- 06-hash-tables-maps-sets/09-two-sum/two-sum.js
+++ 06-hash-tables-maps-sets/09-two-sum/two-sum.js
@@ -1,3 +1,12 @@
-function twoSum() {}
+function twoSum(arr, target) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (j === i) continue
+      else if (arr[j] + arr[i] === target) return [i, j]
+    }
+  }
 
-module.exports = twoSum;
+  return []
+}
+
+module.exports = twoSum
