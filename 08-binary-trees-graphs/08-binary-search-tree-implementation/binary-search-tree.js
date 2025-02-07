6f7325479c7aa498898e8e9b8541924001273317
Yasin Cakal <whoisyasin@gmail.com>
2025-02-07 15:10:22 -0800

    ccc

--- 08-binary-trees-graphs/08-binary-search-tree-implementation/binary-search-tree.js
+++ 08-binary-trees-graphs/08-binary-search-tree-implementation/binary-search-tree.js
@@ -10,6 +10,32 @@ class BinarySearchTree {
   constructor() {
     this.root = null
   }
+
+  insert(value) {
+    const newNode = new Node(value)
+
+    if (this.root === null) {
+      this.root = value
+    } else {
+      let currentNode = this.root
+
+      while (true) {
+        if (value < currentNode.value) {
+          if (!currentNode.left) {
+            currentNode.left = newNode
+            return this
+          }
+          currentNode = currentNode.left
+        } else {
+          if (!currentNode.right) {
+            currentNode.right = newNode
+            return this
+          }
+          currentNode = currentNode.right
+        }
+      }
+    }
+  }
 }
 
 module.exports = { Node, BinarySearchTree }
